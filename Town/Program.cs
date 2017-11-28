05a6cbaa350f124bbee8ba1a926d30e3130002a3
Rune Grimstad <rag@rag.no>
2017-11-28 21:38:52 +0100

    Initial commit

---
+++ Town/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Town.Geom;
+
+namespace Town
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+
+            if (args.Any() && int.TryParse(args[0], out var seed))
+            {
+                Rnd.Seed = seed;
+            }
+
+            var nPatches = 35;
+
+            var town = new Town(nPatches);
+
+            var img = new TownRenderer(town, new TownRendererOptions{RenderOverlay = false}).DrawTown();
+
+            File.WriteAllText(@"C:\temp\town.svg", img);
+        }
+
+  
+    }
+}
